e01e88ec9014024d74fd6c4785ebfdea89a719cd
Abouzar <sadeghi92515@gmail.com>
2023-06-05 23:13:27 +0330

    Start Project

---
+++ src/components/Product/Product.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Product = () => {
+    return (
+        <div id="productID" className="ProductCN">
+            <h3>Title</h3>
+            <p>Product Descriptions</p>
+        </div>
+    )
+}
+
+export default Product
